ba9f8545105474016fbdf5dc2b973e1a3005c4ea
Pgalante <pgalante@amnh.org>
2018-06-05 10:39:06 -0400

    ready for comparisons

--- Turks.R
+++ Turks.R
@@ -1,11 +1,14 @@
-library(dismo)
 library(spocc)
-library(rgeos)
+library(ENMeval)
 library(dplyr)
+library(rgeos)
 library(sp)
 library(spThin)
 library(scrubr)
-library(ENMeval)
+
+
+library(dismo)
+
 ## Get Species name
 species<-"Meleagris gallopavo"
 ## Get subspecies names
@@ -24,8 +27,7 @@ for (i in 1:length(Turks)){
   Turks[[i]]<-unique(na.omit(data.frame(occ2df(Turks[[i]]))))[,1:3]
 }
 names(Turks)<-subspecies
-
-## Scrub data localities
+# Scrub data localities
 scrubTurks <- lapply(Turks, function(x) dframe(x %>% coord_unlikely()))
 
 ## Thin
@@ -42,17 +44,14 @@ mcp <- function (xy) {
   coords.t <- chull(xy[, 1], xy[, 2])
   xy.bord <- xy[coords.t, ]
   xy.bord <- rbind(xy.bord[nrow(xy.bord), ], xy.bord)
-  return(SpatialPolygons(list(Polygons(list(Polygon(as.matrix(xy.bord))), 1))))
+  return(gBuffer(SpatialPolygons(list(Polygons(list(Polygon(as.matrix(xy.bord))), 1))), width = 1))
 }
 # Create hulls for each subspecies
 hulls <- lapply(thinTurks, function(x) mcp(x))
 # Mask env by each of the hulls. List of rasterStacks
 turkEnv <- lapply(hulls, function(x) mask(env, x))
 # Crop environmental data from global to local
-turkCrop <- list()
-for (i in 1:length(turkEnv)){
-  turkCrop[[i]] <- crop(turkEnv[[i]], extent(hulls[[i]]))
-}
+turkCrop<-lapply(1:length(turkEnv), function(x) crop(turkEnv[[x]], hulls[[x]]))
 
 ## Model
 turkRes<-list()
@@ -61,20 +60,39 @@ for (i in 1:length(turkCrop)){
               fc=c("L", "LQ", "H"), RMvalues=seq(0.5,4,0.5), rasterPreds=F)@results
 }
 
-sum(is.na(extract(x = turkCrop[[1]], y = thinTurks[[1]][[1]])))
-plot(turkCrop[[1]][[1]])
-points(thinTurks[[1]][[1]])
+resTable<-turkRes[[1]]
 
-
-<- lapply(thinTurks, function(x) ENMevaluate(occ=data.frame(x), env = turkCrop, method='block', parallel=T, numCores=4,
-                                            fc=c("L", "LQ", "H"), RMvalues=seq(0.5,4,0.5), rasterPreds=F))
-
-test<-function(x,y){
-  Turkresults<-ENMevaluate(occ=data.frame(x), env = y, method='block', parallel=T, numCores=4,
-                        fc=c("L", "LQ", "H"), RMvalues=seq(0.5,4,0.5), rasterPreds=F)
-  return(Turkresults@results)
+## Select optimal model and create logistic predictions
+optimize<- function(mod.Table){
+  no.zero.param <- mod.Table[mod.Table$nparam != 0,]
+  bestOR = no.zero.param[order(no.zero.param[,'Mean.ORmin']),]
+  bestAUC = bestOR[order(bestOR[,'Mean.AUC'], decreasing=TRUE),]
+  opt.mod = bestAUC[1,]
+  return(opt.mod)
 }
-test(thinTurks[[1]], turkEnv[[1]])
 
-Turkresults<-ENMevaluate(occ=data.frame(thinTurks[[1]]), env = turkEnv[[1]], method='block', parallel=T, numCores=4,
-                         fc=c("L", "LQ", "H"), RMvalues=seq(0.5,4,0.5), rasterPreds=F, bg.coords = NULL)
+mod.Table<-turkRes[[6]]
+
+m<-list()
+pred<-list()
+opt.mod<-list()
+for (i in 1:length(turkRes)){
+  opt.mod[[i]]<-optimize(turkRes[[i]])
+  b.m<-opt.mod[[i]]$rm
+  beta.mulr<- paste('betamultiplier=',b.m,sep='')
+  false.args<-c('noautofeature','noproduct','nothreshold','noquadratic','nohinge','nolinear')
+  feat<-strsplit(as.vector(opt.mod[[i]][,2]), ',')[[1]]
+  if (feat == 'L'){feats = 'linear'} else if (feat == 'LQ'){feats = c('quadratic', 'linear')} else if (feat == 'H'){feats = 'hinge'} else if (feat == 'P'){feats = 'product'} else if (feat == 'T'){feats = 'threshold'} else if (feat == 'LQH'){feats = c('linear', 'quadratic', 'hinge')} else if (feat == 'LQHP'){feats = c('linear', 'quadratic', 'hinge', 'product')} else if (feat == 'LQHPT'){feats = c('linear', 'quadratic', 'hinge', 'product', 'threshold')}
+  for (j in 1:length(feats)){false.args[which(sub('no','',false.args)==feats[j])] = feats[j]}
+  m[[i]] <-maxent(turkCrop[[i]], thinTurks[[i]][[1]], args=c(false.args, beta.mulr, 'noremoveduplicates', 'noaddsamplestobackground'))
+  pred[[i]]  <- predict(object= m[[i]], x=turkCrop[[i]], na.rm=TRUE, format='GTiff',overwrite=TRUE, progress='text',args='logistic')
+}
+
+## Threshold predictions to 10% OR
+modThreshs <- lapply(opt.mod, function(x) return(x$Mean.OR10))
+for (i in 1:length(modThreshs)){
+  pred[[i]][pred[[i]] < modThreshs[[i]]] <- NA
+}
+
+## Compare niches
+
